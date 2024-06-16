20a47552ba331353a82eb98e6c7e7ac3e6d6bfc6
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-06-16 12:00:55 +0300

    update style in file indexjs

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,10 @@ export default function Home() {
       } dark:bg-gray-900 min-h-screen`}
     > */}
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Theme Context Example</h1>
-        <p className="mb-4">
+        <h1 className="text-3xl font-bold mb-4 dark:text-white">
+          Theme Context Example
+        </h1>
+        <p className="mb-4 dark:text-slate-400">
           Current Theme: <strong>{darkMode ? "Dark" : "Light"}</strong>
         </p>
         <button
